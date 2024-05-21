dcd76217bbdccacacbecc88ab5a08fa92b84a08b
lukehayes <lukepurplehayes@gmail.com>
2024-05-21 13:42:30 +0100

    Added SiteController::index() and SiteController::other() for testing.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::get('/', [SiteController::class, 'index']);
+Route::get('/other', [SiteController::class, 'other']);
