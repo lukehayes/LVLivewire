6fbdeec6fdc3297323bd82af1bdc5c7ed4c353b9
lukehayes <lukepurplehayes@gmail.com>
2024-05-21 17:36:57 +0100

    Added file version of  tailwind.min.js

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        <script src="https://cdn.tailwindcss.com"></script>
+        <script type="text/javascript" src="{{asset('tailwind.min.js') }}"></script>
     </head>
 
     <body class="">
