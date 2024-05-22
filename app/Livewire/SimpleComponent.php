cec48ee18c5eff94cd68c0476e142bc7592cc8b8
lukehayes <lukepurplehayes@gmail.com>
2024-05-22 09:33:21 +0100

    All posts are now sent to simple component.

--- app/Livewire/SimpleComponent.php
+++ app/Livewire/SimpleComponent.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class SimpleComponent extends Component
 {
+    public $posts;
+
     public function render()
     {
         return view('livewire.simple-component');
+    }
+
+    public function mount()
+    {
+        $this->posts = \App\Models\Post::all();
     }
 }
--- resources/views/livewire/simple-component.blade.php
+++ resources/views/livewire/simple-component.blade.php
@@ -1,4 +1,8 @@
 <div class='p-4 bg-gray-300'>
     {{-- The Master doesn't talk, he acts. --}}
     <p>Simple Component Text.</p>
+
+    @foreach($posts as $post)
+        <p class=''>{{$post->title}}</p>
+    @endforeach($posts as $post)
 </div>
