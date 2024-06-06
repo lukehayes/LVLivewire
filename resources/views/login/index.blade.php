ce98cd1823eeb8318211ecab89f3a103512d01c8
lukehayes <lukepurplehayes@gmail.com>
2024-06-06 17:13:08 +0100

    Updated form styling

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,46 +2,65 @@
 
 @section('login-form')
 
-<div class='flex flex-col items-center justify-center mx-auto'>
+<section class=" dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
 
     <a href="#" class="flex justify-center items-center dark:text-white my-10">
         <img class="w-16 h-16 mr-2 text-red" src="https://img.icons8.com/?size=100&id=33605&format=png&color=000000 alt="logo">
     </a>
 
-    <form action="/login" method="POST" class='bg-white p-10 md:w-96'>
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
 
-        <div class="p-6 space-y-2 md:space-y-6 sm:p-8">
-            <h3 class="font-bold leading-tight tracking-tight text-gray-900">
-                Sign in to your account
-            </h3>
-        </div>
+              <form class="space-y-4 md:space-y-6" action="/login" method="POST">
 
-        @csrf
+                @csrf
 
-        <div class="mb-4">
+            @if ($errors->any())
+                <div class="space-y-2">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class='rounded-lg bg-red-100 p-2 my-4'>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-            @if($errors->any())
 
-                @foreach($errors->all() as $error)
-                    <p class='block bg-red-200 p-4 m-2'> {{$error}} </p>
-                @endforeach()
+                  <div>
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                          <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                          </div>
+                          <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                          </div>
+                      </div>
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  </div>
 
-            @endif()
+                  <button type="submit" class="w-full text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-indigo-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
 
-            <label for="Username" class='block font-bold mb-2 '>Username</label>
-            <input type="text" name='email' placeholder="Username" class='w-full border shadow py-2 px-4'>
-        </div>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
 
-        <div class="mb-4">
-            <label for="Password" class='block font-bold mb-2'>Password</label>
-            <input type="password" name='password' placeholder="Password" class='border shadow w-full py-2 px-4'>
-        </div>
 
-        <div class="mt-4">
-            <input type="submit" value='Login' class='w-full block bg-indigo-500 text-white rounded p-2 hover:bg-indigo-700'>
-        </div>
 
-    </form>
-
-</div>
 @endsection
